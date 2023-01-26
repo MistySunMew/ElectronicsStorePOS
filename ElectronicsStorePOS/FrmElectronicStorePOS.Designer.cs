0398122fdcdea257fb373975405e9be28dac67e7
Zalman Izak <thezalmanian@gmail.com>
2023-01-25 17:08:24 -0800

    Added button to open Create Product form on main form

--- ElectronicsStorePOS/FrmElectronicStorePOS.Designer.cs
+++ ElectronicsStorePOS/FrmElectronicStorePOS.Designer.cs
@@ -28,19 +28,33 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+            this.btnOpenCreateProductForm = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnOpenCreateProductForm
+            // 
+            this.btnOpenCreateProductForm.Location = new System.Drawing.Point(408, 124);
+            this.btnOpenCreateProductForm.Name = "btnOpenCreateProductForm";
+            this.btnOpenCreateProductForm.Size = new System.Drawing.Size(105, 40);
+            this.btnOpenCreateProductForm.TabIndex = 0;
+            this.btnOpenCreateProductForm.Text = "Create Product";
+            this.btnOpenCreateProductForm.UseVisualStyleBackColor = true;
+            this.btnOpenCreateProductForm.Click += new System.EventHandler(this.BtnOpenCreateProductForm_Click);
             // 
             // FrmElectronicStorePOS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpenCreateProductForm);
             this.Name = "FrmElectronicStorePOS";
             this.Text = "Electronic Store POS";
             this.ResumeLayout(false);
 
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private Button btnOpenCreateProductForm;
+    }
 }
--- ElectronicsStorePOS/FrmElectronicStorePOS.cs
+++ ElectronicsStorePOS/FrmElectronicStorePOS.cs
@@ -6,5 +6,18 @@ namespace ElectronicsStorePOS
 		{
 			InitializeComponent();
 		}
-	}
+
+        /// <summary>
+        /// When the "Create Product" button is clicked, creates 
+        /// and displays an instance of the CreateProduct form
+        /// </summary>
+        private void BtnOpenCreateProductForm_Click(object sender, EventArgs e)
+        {
+            // Create new instance of form
+            FrmCreateProduct createProductForm = new();
+
+            // Display it to the user
+            createProductForm.ShowDialog();
+        }
+    }
 }
