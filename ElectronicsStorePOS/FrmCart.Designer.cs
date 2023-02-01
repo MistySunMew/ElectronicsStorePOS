efac7eb55e80d824b0e5e7cc07c586f375863045
Zalman Izak <thezalmanian@gmail.com>
2023-01-31 18:24:55 -0800

    Added necessary controls to Cart form

--- ElectronicsStorePOS/FrmCart.Designer.cs
+++ ElectronicsStorePOS/FrmCart.Designer.cs
@@ -28,19 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lstProductsInCart = new System.Windows.Forms.ListBox();
+            this.lblSubtotal = new System.Windows.Forms.Label();
+            this.txtSubtotal = new System.Windows.Forms.TextBox();
+            this.txtTax = new System.Windows.Forms.TextBox();
+            this.lblTax = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.lblTotal = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lstProductsInCart
+            // 
+            this.lstProductsInCart.FormattingEnabled = true;
+            this.lstProductsInCart.ItemHeight = 15;
+            this.lstProductsInCart.Location = new System.Drawing.Point(23, 22);
+            this.lstProductsInCart.Name = "lstProductsInCart";
+            this.lstProductsInCart.Size = new System.Drawing.Size(273, 154);
+            this.lstProductsInCart.TabIndex = 4;
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.AutoSize = true;
+            this.lblSubtotal.Location = new System.Drawing.Point(380, 46);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(54, 15);
+            this.lblSubtotal.TabIndex = 5;
+            this.lblSubtotal.Text = "Subtotal:";
+            // 
+            // txtSubtotal
+            // 
+            this.txtSubtotal.Enabled = false;
+            this.txtSubtotal.Location = new System.Drawing.Point(440, 43);
+            this.txtSubtotal.Name = "txtSubtotal";
+            this.txtSubtotal.Size = new System.Drawing.Size(100, 23);
+            this.txtSubtotal.TabIndex = 6;
+            this.txtSubtotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTax
+            // 
+            this.txtTax.Enabled = false;
+            this.txtTax.Location = new System.Drawing.Point(440, 72);
+            this.txtTax.Name = "txtTax";
+            this.txtTax.Size = new System.Drawing.Size(100, 23);
+            this.txtTax.TabIndex = 8;
+            this.txtTax.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblTax
+            // 
+            this.lblTax.AutoSize = true;
+            this.lblTax.Location = new System.Drawing.Point(407, 75);
+            this.lblTax.Name = "lblTax";
+            this.lblTax.Size = new System.Drawing.Size(27, 15);
+            this.lblTax.TabIndex = 7;
+            this.lblTax.Text = "Tax:";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Enabled = false;
+            this.txtTotal.Location = new System.Drawing.Point(440, 101);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 23);
+            this.txtTotal.TabIndex = 10;
+            this.txtTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(399, 104);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(35, 15);
+            this.lblTotal.TabIndex = 9;
+            this.lblTotal.Text = "Total:";
             // 
             // FrmCart
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.txtTax);
+            this.Controls.Add(this.lblTax);
+            this.Controls.Add(this.txtSubtotal);
+            this.Controls.Add(this.lblSubtotal);
+            this.Controls.Add(this.lstProductsInCart);
             this.Name = "FrmCart";
             this.Text = "FrmCart";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private ListBox lstProductsInCart;
+        private Label lblSubtotal;
+        private TextBox txtSubtotal;
+        private TextBox txtTax;
+        private Label lblTax;
+        private TextBox txtTotal;
+        private Label lblTotal;
     }
 }
