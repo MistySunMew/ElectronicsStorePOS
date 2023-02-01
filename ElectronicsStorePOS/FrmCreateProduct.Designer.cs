d20c9f09209e83bd6a89a7bbd4329469b0756c70
MistySunMew <Michael.e.forbes@comcast.com>
2023-02-01 14:38:04 -0800

    Updated FrmCreateProduct layout

--- ElectronicsStorePOS/FrmCreateProduct.Designer.cs
+++ ElectronicsStorePOS/FrmCreateProduct.Designer.cs
@@ -28,215 +28,227 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtProductSKU = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.cbxProductCategory = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.cbxGameRating = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.txtProductDesc = new System.Windows.Forms.TextBox();
             this.txtProductName = new System.Windows.Forms.TextBox();
             this.txtProductPrice = new System.Windows.Forms.TextBox();
-            this.txtProductDesc = new System.Windows.Forms.TextBox();
-            this.cbxGameRating = new System.Windows.Forms.ComboBox();
-            this.cbxProductCategory = new System.Windows.Forms.ComboBox();
-            this.txtProductSKU = new System.Windows.Forms.TextBox();
-            this.btnCreateProduct = new System.Windows.Forms.Button();
-            this.btnClearForm = new System.Windows.Forms.Button();
             this.btnReturnHome = new System.Windows.Forms.Button();
+            this.btnClearForm = new System.Windows.Forms.Button();
+            this.btnCreateProduct = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtProductSKU);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cbxProductCategory);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.cbxGameRating);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtProductDesc);
+            this.groupBox1.Controls.Add(this.txtProductName);
+            this.groupBox1.Controls.Add(this.txtProductPrice);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(373, 237);
+            this.groupBox1.TabIndex = 34;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtProductSKU
+            // 
+            this.txtProductSKU.Location = new System.Drawing.Point(194, 187);
+            this.txtProductSKU.Margin = new System.Windows.Forms.Padding(2);
+            this.txtProductSKU.Name = "txtProductSKU";
+            this.txtProductSKU.Size = new System.Drawing.Size(146, 27);
+            this.txtProductSKU.TabIndex = 26;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(121, 21);
+            this.label1.Location = new System.Drawing.Point(141, 28);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Name";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Name:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(129, 54);
+            this.label2.Location = new System.Drawing.Point(149, 60);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 20);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Price";
+            this.label2.Size = new System.Drawing.Size(44, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Price:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(86, 89);
+            this.label3.Location = new System.Drawing.Point(105, 92);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 20);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Description";
+            this.label3.Size = new System.Drawing.Size(88, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Description:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 153);
+            this.label4.Location = new System.Drawing.Point(22, 157);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(168, 20);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Rating (Only for Games)";
+            this.label4.Size = new System.Drawing.Size(171, 20);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Rating (Only for Games):";
+            // 
+            // cbxProductCategory
+            // 
+            this.cbxProductCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxProductCategory.FormattingEnabled = true;
+            this.cbxProductCategory.Location = new System.Drawing.Point(194, 121);
+            this.cbxProductCategory.Margin = new System.Windows.Forms.Padding(2);
+            this.cbxProductCategory.Name = "cbxProductCategory";
+            this.cbxProductCategory.Size = new System.Drawing.Size(146, 28);
+            this.cbxProductCategory.TabIndex = 25;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(101, 118);
+            this.label5.Location = new System.Drawing.Point(121, 124);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(69, 20);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Category";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(133, 182);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(36, 20);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "SKU";
-            // 
-            // txtProductName
-            // 
-            this.txtProductName.Location = new System.Drawing.Point(190, 16);
-            this.txtProductName.Margin = new System.Windows.Forms.Padding(2);
-            this.txtProductName.Name = "txtProductName";
-            this.txtProductName.Size = new System.Drawing.Size(146, 27);
-            this.txtProductName.TabIndex = 6;
-            // 
-            // txtProductPrice
-            // 
-            this.txtProductPrice.Location = new System.Drawing.Point(190, 50);
-            this.txtProductPrice.Margin = new System.Windows.Forms.Padding(2);
-            this.txtProductPrice.Name = "txtProductPrice";
-            this.txtProductPrice.Size = new System.Drawing.Size(146, 27);
-            this.txtProductPrice.TabIndex = 7;
-            // 
-            // txtProductDesc
-            // 
-            this.txtProductDesc.Location = new System.Drawing.Point(190, 84);
-            this.txtProductDesc.Margin = new System.Windows.Forms.Padding(2);
-            this.txtProductDesc.Name = "txtProductDesc";
-            this.txtProductDesc.Size = new System.Drawing.Size(146, 27);
-            this.txtProductDesc.TabIndex = 8;
+            this.label5.Size = new System.Drawing.Size(72, 20);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Category:";
             // 
             // cbxGameRating
             // 
             this.cbxGameRating.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbxGameRating.Enabled = false;
             this.cbxGameRating.FormattingEnabled = true;
-            this.cbxGameRating.Location = new System.Drawing.Point(190, 146);
+            this.cbxGameRating.Location = new System.Drawing.Point(194, 154);
             this.cbxGameRating.Margin = new System.Windows.Forms.Padding(2);
             this.cbxGameRating.Name = "cbxGameRating";
             this.cbxGameRating.Size = new System.Drawing.Size(146, 28);
-            this.cbxGameRating.TabIndex = 9;
+            this.cbxGameRating.TabIndex = 24;
             // 
-            // cbxProductCategory
+            // label6
             // 
-            this.cbxProductCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbxProductCategory.FormattingEnabled = true;
-            this.cbxProductCategory.Location = new System.Drawing.Point(190, 116);
-            this.cbxProductCategory.Margin = new System.Windows.Forms.Padding(2);
-            this.cbxProductCategory.Name = "cbxProductCategory";
-            this.cbxProductCategory.Size = new System.Drawing.Size(146, 28);
-            this.cbxProductCategory.TabIndex = 10;
-            this.cbxProductCategory.SelectedIndexChanged += new System.EventHandler(this.cbxProductCategory_SelectedIndexChanged);
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(153, 190);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 20);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "SKU:";
             // 
-            // txtProductSKU
+            // txtProductDesc
             // 
-            this.txtProductSKU.Location = new System.Drawing.Point(190, 178);
-            this.txtProductSKU.Margin = new System.Windows.Forms.Padding(2);
-            this.txtProductSKU.Name = "txtProductSKU";
-            this.txtProductSKU.Size = new System.Drawing.Size(146, 27);
-            this.txtProductSKU.TabIndex = 11;
+            this.txtProductDesc.Location = new System.Drawing.Point(194, 89);
+            this.txtProductDesc.Margin = new System.Windows.Forms.Padding(2);
+            this.txtProductDesc.Name = "txtProductDesc";
+            this.txtProductDesc.Size = new System.Drawing.Size(146, 27);
+            this.txtProductDesc.TabIndex = 23;
             // 
-            // btnCreateProduct
+            // txtProductName
             // 
-            this.btnCreateProduct.Location = new System.Drawing.Point(24, 214);
-            this.btnCreateProduct.Margin = new System.Windows.Forms.Padding(2);
-            this.btnCreateProduct.Name = "btnCreateProduct";
-            this.btnCreateProduct.Size = new System.Drawing.Size(120, 53);
-            this.btnCreateProduct.TabIndex = 12;
-            this.btnCreateProduct.Text = "Create Product";
-            this.btnCreateProduct.UseVisualStyleBackColor = true;
-            this.btnCreateProduct.Click += new System.EventHandler(this.btnCreateProduct_Click);
+            this.txtProductName.Location = new System.Drawing.Point(194, 25);
+            this.txtProductName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(146, 27);
+            this.txtProductName.TabIndex = 21;
+            // 
+            // txtProductPrice
+            // 
+            this.txtProductPrice.Location = new System.Drawing.Point(194, 57);
+            this.txtProductPrice.Margin = new System.Windows.Forms.Padding(2);
+            this.txtProductPrice.Name = "txtProductPrice";
+            this.txtProductPrice.Size = new System.Drawing.Size(146, 27);
+            this.txtProductPrice.TabIndex = 22;
+            // 
+            // btnReturnHome
+            // 
+            this.btnReturnHome.Location = new System.Drawing.Point(438, 108);
+            this.btnReturnHome.Margin = new System.Windows.Forms.Padding(2);
+            this.btnReturnHome.Name = "btnReturnHome";
+            this.btnReturnHome.Size = new System.Drawing.Size(120, 53);
+            this.btnReturnHome.TabIndex = 33;
+            this.btnReturnHome.Text = "Return Home";
+            this.btnReturnHome.UseVisualStyleBackColor = true;
+            this.btnReturnHome.Click += new System.EventHandler(this.btnReturnHome_Click);
             // 
             // btnClearForm
             // 
-            this.btnClearForm.Location = new System.Drawing.Point(187, 214);
+            this.btnClearForm.Location = new System.Drawing.Point(438, 186);
             this.btnClearForm.Margin = new System.Windows.Forms.Padding(2);
             this.btnClearForm.Name = "btnClearForm";
             this.btnClearForm.Size = new System.Drawing.Size(120, 53);
-            this.btnClearForm.TabIndex = 13;
+            this.btnClearForm.TabIndex = 32;
             this.btnClearForm.Text = "Clear Form";
             this.btnClearForm.UseVisualStyleBackColor = true;
             this.btnClearForm.Click += new System.EventHandler(this.btnClearForm_Click);
             // 
-            // btnReturnHome
+            // btnCreateProduct
             // 
-            this.btnReturnHome.Location = new System.Drawing.Point(370, 21);
-            this.btnReturnHome.Margin = new System.Windows.Forms.Padding(2);
-            this.btnReturnHome.Name = "btnReturnHome";
-            this.btnReturnHome.Size = new System.Drawing.Size(120, 53);
-            this.btnReturnHome.TabIndex = 14;
-            this.btnReturnHome.Text = "Return Home";
-            this.btnReturnHome.UseVisualStyleBackColor = true;
-            this.btnReturnHome.Click += new System.EventHandler(this.btnReturnHome_Click);
+            this.btnCreateProduct.Location = new System.Drawing.Point(438, 30);
+            this.btnCreateProduct.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCreateProduct.Name = "btnCreateProduct";
+            this.btnCreateProduct.Size = new System.Drawing.Size(120, 53);
+            this.btnCreateProduct.TabIndex = 31;
+            this.btnCreateProduct.Text = "Create Product";
+            this.btnCreateProduct.UseVisualStyleBackColor = true;
+            this.btnCreateProduct.Click += new System.EventHandler(this.btnCreateProduct_Click);
             // 
             // FrmCreateProduct
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(501, 284);
+            this.ClientSize = new System.Drawing.Size(574, 277);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnReturnHome);
             this.Controls.Add(this.btnClearForm);
             this.Controls.Add(this.btnCreateProduct);
-            this.Controls.Add(this.txtProductSKU);
-            this.Controls.Add(this.cbxProductCategory);
-            this.Controls.Add(this.cbxGameRating);
-            this.Controls.Add(this.txtProductDesc);
-            this.Controls.Add(this.txtProductPrice);
-            this.Controls.Add(this.txtProductName);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "FrmCreateProduct";
             this.Text = "Create a Product";
             this.Load += new System.EventHandler(this.FrmCreateProduct_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private GroupBox groupBox1;
+        private TextBox txtProductSKU;
         private Label label1;
         private Label label2;
         private Label label3;
         private Label label4;
+        private ComboBox cbxProductCategory;
         private Label label5;
+        private ComboBox cbxGameRating;
         private Label label6;
+        private TextBox txtProductDesc;
         private TextBox txtProductName;
         private TextBox txtProductPrice;
-        private TextBox txtProductDesc;
-        private ComboBox cbxGameRating;
-        private ComboBox cbxProductCategory;
-        private TextBox txtProductSKU;
-        private Button btnCreateProduct;
-        private Button btnClearForm;
         private Button btnReturnHome;
+        private Button btnClearForm;
+        private Button btnCreateProduct;
     }
 }
